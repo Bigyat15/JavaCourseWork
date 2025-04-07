bb2ac25419f2dd313c57fe10295642a8f9cf0e78
Bigyat15 <noobbigyat@gmail.com>
2025-04-07 18:17:59 +0545

    DAO file created and written logic but sql query is yet to be put

--- src/main/java/DAO/registerDAO.java
+++ src/main/java/DAO/registerDAO.java
@@ -1,5 +1,42 @@
 package DAO;
 
-public class registerDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import database.DatabaseConnection;
+import models.registerModel;
+
+public class registerDAO {
+	private Connection con;
+	
+	public registerDAO() {
+		try {
+			this.con = DatabaseConnection.getConnection();
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public boolean addRegistrationDetail(registerModel register) throws SQLException {
+		boolean hasInserted = false;
+		if(con == null) {
+			System.out.println("Nothing has been inserted.");
+			return hasInserted;
+		}
+		String query = "";
+		try(PreparedStatement ps = con.prepareStatement(query)){
+			ps.setString(1, register.getFirst_name());
+			ps.setString(2,register.getLast_name());
+			ps.setString(3, register.getDOB());
+			ps.setString(4,register.getEmail());
+			ps.setString(5, register.getPhoneNumber());
+			ps.setString(6, register.getPassword());
+			
+			hasInserted = ps.executeUpdate() > 0;
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return hasInserted;
+	}
 }
