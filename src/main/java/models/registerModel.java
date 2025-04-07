3b7d74ddb8490155fdeb8131fe5a948ef7682d9a
Bigyat15 <noobbigyat@gmail.com>
2025-04-07 17:40:47 +0545

    Successfully created and written the code for models/registerModel.java

---
+++ src/main/java/models/registerModel.java
@@ -0,0 +1,88 @@
+package models;
+
+import java.time.LocalDateTime;
+
+public class registerModel {
+	private String first_name ;
+	private String last_name;
+	private String DOB;
+	private String email;
+	private String phoneNumber;
+	private boolean isAdmin;
+	private LocalDateTime created_at;
+	private LocalDateTime updated_at;
+	public registerModel(String first_name, String last_name, String dOB, String email, String phoneNumber,
+			boolean isAdmin, LocalDateTime created_at, LocalDateTime updated_at) {
+		super();
+		this.first_name = first_name;
+		this.last_name = last_name;
+		DOB = dOB;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.isAdmin = isAdmin;
+		this.created_at = LocalDateTime.now();
+		this.updated_at = LocalDateTime.now();
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+		updateTimestamps();
+		
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+		updateTimestamps();
+	}
+	public String getDOB() {
+		return DOB;
+	}
+	public void setDOB(String dOB) {
+		DOB = dOB;
+		updateTimestamps();
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+		updateTimestamps();
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+		updateTimestamps();
+	}
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+		updateTimestamps();
+	}
+	public LocalDateTime getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(LocalDateTime created_at) {
+		this.created_at = created_at;
+		updateTimestamps();
+	}
+	public LocalDateTime getUpdated_at() {
+		return updated_at;
+	}
+	public void setUpdated_at(LocalDateTime updated_at) {
+		this.updated_at = updated_at;
+	}
+	
+	public void updateTimestamps() {
+		this.updated_at = LocalDateTime.now();
+	}
+	
+
+}
